5100628817b88cd10d226369c8ddebaefb0d5895
海哥 <2801359160@qq.com>
2026-03-18 17:36:28 +0800

    修复配置文件路径

---
+++ config/cache.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+return [
+    'default' => env('CACHE_DRIVER', 'file'),
+    'stores' => [
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+        ],
+        'file' => [
+            'driver' => 'file',
+            'path' => storage_path('framework/cache/data'),
+        ],
+    ],
+    'prefix' => env('CACHE_PREFIX', 'ai_side'),
+];
